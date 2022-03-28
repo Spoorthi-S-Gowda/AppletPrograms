889689038b145c53ad37d135226c73814d860fdf
spoorthi gowda <spoorthigowda504@gmail.com>
2022-03-28 07:25:20 +0530

    13th commit

---
+++ JavaApplet/src/Day13/Applet1.java
@@ -0,0 +1,27 @@
+package Day13;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Applet1 extends Applet
+{
+	public void init()
+	{
+	System.out.println("Initializing an applet");
+	}
+
+	public void start()
+	{
+	System.out.println("Starting an applet");
+	}
+
+	public void stop()
+	{
+	System.out.println("Stopping an applet");
+	}
+
+	public void destroy()
+	{
+	System.out.println("Destroying an applet");
+	}
+}
