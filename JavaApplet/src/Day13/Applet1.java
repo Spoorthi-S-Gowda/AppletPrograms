7947a7e550fa0505551f2886816a30407e6e4cb3
spoorthi gowda <spoorthigowda504@gmail.com>
2022-04-10 11:28:32 +0530

    13th commit

--- JavaApplet/src/Day13/Applet1.java
+++ JavaApplet/src/Day13/Applet1.java
@@ -1,27 +1,21 @@
 package Day13;
 
-import java.awt.*;
 import java.applet.*;
+import java.awt.Dimension;
+import java.awt.*;
+
 
 public class Applet1 extends Applet
 {
+	Button b;
+	TextField t;
 	public void init()
 	{
-	System.out.println("Initializing an applet");
-	}
-
-	public void start()
-	{
-	System.out.println("Starting an applet");
-	}
-
-	public void stop()
-	{
-	System.out.println("Stopping an applet");
-	}
-
-	public void destroy()
-	{
-	System.out.println("Destroying an applet");
+		b= new Button("SUBMIT");
+		//b.setLabel("SUBMIT");
+		add(b);
+		t= new TextField();	
+		t.setSize(300,250);
+		add(t);			
 	}
 }
